84f809de17a5084ecf4d609b7696f0269875783a
supratik jana <jana.supratik@senrysa.com>
2026-06-15 21:04:37 +0530

    some fixes

--- server/index.js
+++ server/index.js
@@ -152,6 +152,13 @@ app.post('/api/enquiry', async (req, res) => {
   }
 });
 
+// Serve built React app
+const DIST_DIR = path.join(__dirname, '../dist');
+app.use(express.static(DIST_DIR));
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(DIST_DIR, 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
