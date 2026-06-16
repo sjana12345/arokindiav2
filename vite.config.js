f3b388def101dcbdae3cabbc845d2027e2e9ef3b
supratik jana <jana.supratik@senrysa.com>
2026-06-16 16:30:52 +0530

    Network dependency tree

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,21 @@ export default defineConfig({
   ],
   build: {
     outDir: 'dist/client',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('/node_modules/react-dom') || id.includes('/node_modules/react/') || id.includes('/node_modules/scheduler')) {
+            return 'react-vendor';
+          }
+          if (id.includes('/node_modules/framer-motion')) {
+            return 'motion';
+          }
+          if (id.includes('/node_modules/react-router') || id.includes('/node_modules/react-scroll')) {
+            return 'router';
+          }
+        },
+      },
+    },
   },
   server: {
     proxy: {
